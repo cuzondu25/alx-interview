a3f07ec9eda3c159f416297eecad76d080a9aee1
cuzondu25 <cuzondu25@gmail.com>
2024-09-09 05:25:25 -0700

    Rotate 2D matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''2D matrix'''
+"""2D matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    ''' A function that rotates a 2d matrix 90° clockwise
+    """ A function that rotates a 2d matrix 90° clockwise
         Returns: Nothing
-    '''
+    """
     left, right = 0, len(matrix) - 1
 
     while left < right:
